c4fc529fa07b51eb8662e47cee25e70e167a4fd3
Tibimac <tibimac@me.com>
2015-08-04 17:22:47 +0200

    Initial Commit

---
+++ WarShips/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WarShips
+//
+//  Created by Thibault Le Cornec on 04/08/15.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WarShips/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WarShips
+//
+//  Created by Thibault Le Cornec on 04/08/15.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
